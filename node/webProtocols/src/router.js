b9c960211eed8b31fe55d9790be5a45e657d4f02
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-06-10 22:13:26 -0300

    add session route and basic jwt controller and config

--- node/webProtocols/src/router.js
+++ node/webProtocols/src/router.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import UserController from './app/controllers/UserControllers'
-import  Database from './database/index'
+import UserController from './app/controllers/UserControllers';
+import SessionController from './app/controllers/SessionControllers';
+import Database from './database/index';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store)
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
