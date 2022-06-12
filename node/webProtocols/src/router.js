bc32bc90517486beea83801c15ad89dcec6f456d
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-06-12 15:47:21 -0300

    add multer config for file upload

--- node/webProtocols/src/router.js
+++ node/webProtocols/src/router.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserControllers';
 import SessionController from './app/controllers/SessionControllers';
 import Database from './database/index';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
@@ -12,5 +15,13 @@ routes.post('/session', SessionController.store);
 // AUTHENTICATION REQUIRED ROUTES
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+// UPLOAD FILES
+
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({
+        message: 'successfull upload'
+    });
+});
 
 export default routes;
